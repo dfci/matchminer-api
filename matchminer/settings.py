2d85f2decb9cc3477133b9d4c627e76fe43477b0
esiegel <esiegel@jimmy.harvard.edu>
2019-01-16 14:12:02 -0500

    remove logging of secrets values

--- matchminer/settings.py
+++ matchminer/settings.py
@@ -61,7 +61,6 @@ if file_path is not None:
         vars_ = json.load(fin)
         for name, value in vars_.iteritems():
             globals()[name] = value
-            logging.warn(str(name) + " | " + str(value))
 
 
 if EMAIL_ACTIVE_PROTECTED == "True":
