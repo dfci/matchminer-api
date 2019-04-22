6a703d096c8da546534168fa0a4c35101d7eabc0
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-04-22 15:26:12 -0400

    Update settings to read ONCOTREE_CUSTOM_DIR from environment variables

--- matchminer/settings_prod.py
+++ matchminer/settings_prod.py
@@ -17,7 +17,7 @@ TOKEN_TIMEOUT = 60
 DATA_DIR = "/mm_staging"
 DATA_CLINICAL_CSV = os.path.join(DATA_DIR, "clinical.pkl")
 DATA_GENOMIC_CSV = os.path.join(DATA_DIR, "genomic.pkl")
-DATA_ONCOTREE_FILE = "/var/www/apache-flask/api/matchminer/data/oncotree_file.txt"
+DATA_ONCOTREE_FILE = os.getenv("ONCOTREE_CUSTOM_DIR", "/var/www/apache-flask/api/matchminer/data/oncotree_file.txt")
 
 # backup directory.
 BACKUP_HOURLY_DIR = os.path.join(BACKUP_DIR, "hourly")
