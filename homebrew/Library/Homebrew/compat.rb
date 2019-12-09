566b3bb5d0cbba2339f3a6c9c1f08373cfe94c79
esiegel <esiegel@jimmy.harvard.edu>
2019-12-09 10:40:35 -0500

    delete homebrew and external git repositories from repo
    Former-commit-id: 01606b335d9a99ad89c148614c30ea68370ee128

--- homebrew/Library/Homebrew/compat.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require "compat/cask/dsl/version"
-require "compat/requirements/macos_requirement"
--- homebrew/Library/Homebrew/extend/os/utils/analytics.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require "extend/os/linux/utils/analytics" if OS.linux?
-require "extend/os/mac/utils/analytics" if OS.mac?
